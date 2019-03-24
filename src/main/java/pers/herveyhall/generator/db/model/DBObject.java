849026e2e3a18464ae1383fd77b0c2f1d0eb9870
HerveyHall <herveyhall@foxmail.com>
2019-03-24 14:08:34 +0800

    DBObject类性能优化
    Signed-off-by: HerveyHall <herveyhall@foxmail.com>

--- src/main/java/pers/herveyhall/generator/db/model/DBObject.java
+++ src/main/java/pers/herveyhall/generator/db/model/DBObject.java
@@ -30,23 +30,23 @@ public class DBObject {
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		StringBuilder stringBuilder = new StringBuilder();
+		StringBuffer stringBuffer = new StringBuffer();
 		Field[] fields = this.getClass().getDeclaredFields();
-		stringBuilder.append("[");
+		stringBuffer.append("[");
 		for (Field field : fields) {
 			field.setAccessible(true);
 			try {
-				stringBuilder.append(null != field.getName() ? field.getName() : null).append(" = ");
-				stringBuilder.append(field.get(this));
+				stringBuffer.append(null != field.getName() ? field.getName() : null).append(" = ");
+				stringBuffer.append(field.get(this));
 			} catch (IllegalArgumentException | IllegalAccessException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			field.setAccessible(false);
-			stringBuilder.append(", ");
+			stringBuffer.append(", ");
 		}
-		stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("]").append("(")
+		stringBuffer.delete(stringBuffer.length() - 2, stringBuffer.length()).append("]").append("(")
 				.append(super.toString()).append(")");
-		return stringBuilder.toString();
+		return stringBuffer.toString();
 	}
 }
